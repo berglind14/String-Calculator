14e0a14a0cdbff2bb0d57a2fc81f4d67692d6f38
Berglind <Berglind>
2016-10-12 23:13:59 +0000

    refactored for multiple numbers - working

--- src/main/java/is/ru/String-Calculator/Calculator.java
+++ src/main/java/is/ru/String-Calculator/Calculator.java
@@ -3,19 +3,28 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		if(text == "")
-			return 0;
-		else if(text.contains(","))
-		{
-			String [] numbers = text.split(",");
-			int total = 0;
-			for(String number : numbers)
-			{
-				total = total + toInt(number);
-			}
-			return total;
+		if(text.equals("")){
+	   		return 0;
 		}
-		else
-			return 1;
+	  	else if(text.contains(",")){
+	    	return sum(splitNumbers(text));
+	  	}
+	  	else
+	    	return 1;
+	}
+
+	private static String[] splitNumbers(String numbers){
+		return numbers.split(",");
+	}
+
+	private static int sum(String[] numbers){
+		int total = 0;
+	    for(String number : numbers){
+	    	total += toInt(number);
+	  	}
+	  	return total;
+	}
+	private static int toInt(String number){
+  		return Integer.parseInt(number);
 	}
 }
