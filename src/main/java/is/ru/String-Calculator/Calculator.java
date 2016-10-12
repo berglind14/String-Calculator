5e385f66bf021e6f596014e2b9cc845d51108e13
Berglind <Berglind>
2016-10-12 23:01:16 +0000

    calculator refactored for testing three numbers - working

--- src/main/java/is/ru/String-Calculator/Calculator.java
+++ src/main/java/is/ru/String-Calculator/Calculator.java
@@ -5,6 +5,11 @@ public class Calculator {
 	public static int add(String text){
 		if(text == "")
 			return 0;
+		else if(text.contains(","))
+		{
+			String [] numbers = text.split(",");
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		}
 		else
 			return 1;
 	}
